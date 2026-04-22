ee7993e1256967216cf41e69aa1d764e5540d57d
Ziyodullokhan <ziyodullokhan009@mail.ru>
2026-04-23 00:13:49 +0900

    fear: develop author router ejs page

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ console.log("Hello Khan");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Krish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2  Session code
 // 3 views code
@@ -26,19 +36,20 @@ app.set("view engine", "ejs");
 //     res.end("<h1> The places that today you have to go!!!</h1>")
 // });
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"})
-})
-
-
-app.get("/", function (req, res) {
-    res.render("harid");
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`)
+  console.log(`The server is running successfully on port: ${PORT}`);
 });
